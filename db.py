c7172616b5c0cde9d6cfe5eebb13c5e2c42e9463
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-07-29 18:48:28 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,13 +2,8 @@ import streamlit as st
 from pymongo import MongoClient
 import certifi
 
-# Use secrets from Streamlit Cloud
 MONGO_URI = st.secrets["MONGO_URI"]
-
-# Connect to MongoDB Atlas
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
-
-# Define your database and collections
 db = client["medicare_agent"]
 patients_collection = db["patients"]
 alerts_collection = db["alerts"]
